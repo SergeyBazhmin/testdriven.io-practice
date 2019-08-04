8eecd89e47a2eba4436543a1498649bc9550c928
SergeyBazhmin <sbazhmin@yandex.ru>
2019-08-04 17:45:52 +0300

    test coverage

--- manage.py
+++ manage.py
@@ -1,18 +1,26 @@
 import unittest
+import coverage
+
 from flask_script import Manager
 
 from project import create_app, db
 from project.api.models import User
 
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/server/config.py',
+        'project/server/*/__init__.py'
+    ]
+)
+COV.start()
+
+
 app = create_app()
 manager = Manager(app)
-
-
-@manager.command
-def recreate_db():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
 
 
 @manager.command
@@ -22,6 +30,28 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+
+@manager.command
+def cov():
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
+
+
+@manager.command
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 
 @manager.command
