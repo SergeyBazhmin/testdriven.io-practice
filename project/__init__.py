bb47da47f9014400e3ba81b2904b8edb14ccb5d8
SergeyBazhmin <sbazhmin@yandex.ru>
2019-08-05 13:22:50 +0300

    cors enabled

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -7,6 +8,8 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    CORS(app)
 
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
